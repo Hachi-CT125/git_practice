755d651b8f1b6d93de4a2306a41203e77162d0e7
Hachi <soulofocean3106@gmail.com>
2025-12-05 15:40:18 +0900

    feat: Add click handler for button B using nth-child(2)

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function sayHello(personName){
-
-    console.log("こんにちは、" + personName + "!")
-}
-
-sayHello("ジェミニ");
-sayHello("太郎");
+const heading = document.querySelector("h1")
+const buttonA = document.querySelector(".button-container .btn:nth-child(1)")
+const buttonB = document.querySelector(".button-container .btn:nth-child(2)")
+buttonA.addEventListener("click", function(){
+    heading.textContent = "ボタンAが押されたよ!";
+    console.log("ボタンクリックAが実行されました。");
+});
+buttonB.addEventListener("click", function(){
+    heading.textContent = "ボタンBが押されたんじゃあ!";
+    console.log("ボタンクリックBが実行されました。")
+})

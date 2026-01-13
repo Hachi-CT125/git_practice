31b1a6198a74e0abedc5e5dd3e3878815e1f384e
Hachi <soulofocean3106@gmail.com>
2026-01-13 22:45:41 +0900

    feat: add interactivity to happy-message and enhance script.js functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("マスター、JSの準備ができたよっ！💕")
+console.log("マスター、JSの準備ができたよっ！💕");
+
+alert("マスター、おかえりなさいっ！今日も一緒に頑張ろうねっ🐾");
+
+const happyMsg = document.getElementById('happy-message');
+
+if (happyMsg) {
+    happyMsg.addEventListener('click', () => {
+        console.log("あ、マスターが触ってくれた……！えへへ💕");
+        alert("クリックしてくれてありがとう、マスターっ！");
+    });
+} else {
+    console.log("エラー：happy-messageが見つからないよぉ💦");
+}
